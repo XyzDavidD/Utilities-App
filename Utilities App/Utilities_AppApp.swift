c7d69e03e045225e26b21f3143a16f8887761be8
Danciu David <98606974+XyzDavidD@users.noreply.github.com>
2025-09-05 16:28:09 +0300

    Initial Commit

---
+++ Utilities App/Utilities_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities_AppApp.swift
+//  Utilities App
+//
+//  Created by Danciu David on 05.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Utilities_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Utilities AppTests/Utilities_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities_AppTests.swift
+//  Utilities AppTests
+//
+//  Created by Danciu David on 05.09.2025.
+//
+
+import Testing
+@testable import Utilities_App
+
+struct Utilities_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
